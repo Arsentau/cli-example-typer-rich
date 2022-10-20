38384759bfe392a8f5405d6bbd23eec645f0c1ac
Nabor Torres <arsentau@gmail.com>
2022-10-20 13:34:04 -0300

    Fix hello command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from entities.currencies import Currency
 from rich.console import Console
 from rich.progress import track
+from rich.style import errors as rich_style_errors
 from services.api_consumer import Exchange
 from services.calculator import Calculator, CalculatorLogger
 from tables.exchange import exchange_table_gen
@@ -13,8 +14,11 @@ app = Typer()
 def hello(name: str, color: str = "yellow"):
     """Returns hello + the given name with rich color styling"""
     console = Console()
-    console.print(f"Hello {name}", style=f"bold {color.lower}")
-
+    try:
+        console.print(f"Hello {name}", style=f"bold {color.lower()}")
+    except rich_style_errors.MissingStyle:
+        console.print(f"Hello {name}")
+        console.print(f"Invalid color: {color}", style="bold red")
 
 @app.command()
 def currencies(
